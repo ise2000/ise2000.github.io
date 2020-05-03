62541eaa35234b947bb68988c109184c05bf99ce
Sergey Ivanov <ise200017@gmail.com>
2020-05-03 17:17:01 +0300

    added pipette, pomodoro improvements

--- scripts/colorPicker.js
+++ scripts/colorPicker.js
@@ -1,10 +1,8 @@
-let red = 0;
-let green = 0;
-let blue = 0;
+let red = 255;
+let green = 255;
+let blue = 255;
 let color = "#000000";
-window.onload = () => {
-    document.getElementById("colorpicker").style.backgroundColor = color;
-}
+
 function redChange(value) {
     red = value;
     combine();
@@ -22,10 +20,24 @@ function blueChange(value) {
 
 function combine() {
     color = "#" + pad(Number.parseInt(red).toString(16)) + pad(Number.parseInt(green).toString(16)) + pad(Number.parseInt(blue).toString(16));
-    document.getElementById("colorpicker").style.backgroundColor = color;
-    console.log(color);
+    //document.getElementById("colorpicker").style.backgroundColor = color;
+    document.body.  style.backgroundColor = color;
+    document.getElementById("colorHex").innerText = color;
 }
 
 function pad(number) {
     return number.toString().length < 2 ? "0" + number : number;
+}
+
+function copyText() {
+    const el = document.createElement('textarea');
+    el.value = color;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert("The color copied to clipboard.");
 }
